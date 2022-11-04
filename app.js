7c233ee938c23822c2aaf01eaf041936ffae8040
Kendall Powell <kendalpowel@hotmail.com>
2022-11-04 16:06:27 -0600

    background + character + total and upgrades linked

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log('js loaded')
+
+let clickUpgrades = [
+  {
+    name: 'Amulet Coin',
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  }
+]
+
+let autoUpgrades = [
+  {
+    name: 'More Pokemon',
+    price: 300,
+    quantity: 0,
+    multiplier: 20
+  }
+]
+
+let clickCynthia = 1
+let total = 0
+
+function addDollar() {
+  // console.log('plus', 1)
+  total += clickCynthia
+  drawTotal()
+}
+
+function drawTotal() {
+  let getTotal = document.getElementById('total')
+  getTotal.innerText = total
+}
+
+function buyAmulet() {
+  console.log('bought upgrade')
+}
+
+function buyMorePokemon() {
+  console.log('bought upgrade')
+}
